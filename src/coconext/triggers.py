a0e194779f34ca7f8a39b1931fda8d684a1fab3d
Kaleb Barrett <dev.ktbarrett@gmail.com>
2025-06-22 16:26:06 -0600

    Remove Lock, cocotb.Lock is fair

--- src/coconext/triggers.py
+++ src/coconext/triggers.py
@@ -2,14 +2,7 @@
 
 from __future__ import annotations
 
-from collections import deque
-from typing import TYPE_CHECKING
-
-from cocotb.task import Task, current_task
 from cocotb.triggers import Event
-
-if TYPE_CHECKING:
-    from typing import Self
 
 
 class Notify:
@@ -39,67 +32,3 @@ class Notify:
     async def wait(self) -> None:
         """Wait until the notify() method is called."""
         await self._event.wait()
-
-
-class Lock:
-    """A fair mutual exclusion lock.
-
-    This functions similarly to :class:`.Lock`, except it designed to be "fair".
-    Tasks are guaranteed to acquire the Lock in the order each attempts to acquire it.
-
-    .. versionadded:: 0.2
-    """
-
-    def __init__(self) -> None:
-        self._locked: bool = False
-        self._acquirers = deque[tuple[Event, Task]]()
-        self._current_acquirer: Task[object]
-
-    def locked(self) -> bool:
-        """Return ``True`` if the lock is *locked*."""
-        return self._locked
-
-    async def acquire(self) -> None:
-        """Acquire the lock.
-
-        Waits until the lock is *unlocked*, sets it to *locked* and returns.
-        Multiple Tasks may attempt to acquire the Lock, but only one will proceed.
-        Tasks are guaranteed to acquire the Lock in the order each attempts to acquire it.
-
-        Raises:
-            RuntimeError: If the Task that has acquired the lock attempt to reacquire it.
-        """
-        if self._locked:
-            acquirer_task = current_task()
-            if acquirer_task is self._current_acquirer:
-                raise RuntimeError(
-                    "Acquiring Task while you already have it is not allowed"
-                )
-            e = Event()
-            self._acquirers.append((e, acquirer_task))
-            await e.wait()
-        self._locked = True
-
-    def release(self) -> None:
-        """Release the lock.
-
-        Sets the lock to *unlocked*.
-
-        Raises:
-            RuntimeError: If called when the lock is *unlocked*.
-        """
-        if not self._locked:
-            raise RuntimeError("Lock is not acquired")
-        self._locked = False
-        while self._acquirers:
-            e, task = self._acquirers.popleft()
-            if not task.done():
-                e.set()
-                return
-
-    async def __aenter__(self) -> Self:
-        await self.acquire()
-        return self
-
-    async def __aexit__(self, *_: object) -> None:
-        self.release()
