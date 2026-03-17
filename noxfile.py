45faddda119228c9fb22e7a37d4ccbf4ccfd9d67
Kaleb Barrett <dev.ktbarrett@gmail.com>
2026-03-16 19:30:06 -0600

    Use nox-uv

--- noxfile.py
+++ noxfile.py
@@ -3,35 +3,37 @@ from __future__ import annotations
 import glob
 
 import nox
+from nox_uv import session
 
-nox.options.default_venv_backend = "uv|virtualenv"
+nox.options.default_venv_backend = "uv"
 
 
-@nox.session
-def docs(session: nox.Session) -> None:
-    session.run("uv", "sync", "--active", "--group", "docs")
-
-    outdir = session.cache_dir / "docs_out"
-    session.run(
+@session(
+    uv_groups=["docs"],
+    uv_sync_locked=False,
+)
+def docs(s: nox.Session) -> None:
+    outdir = s.cache_dir / "docs_out"
+    s.run(
         "sphinx-build",
         "./docs",
         str(outdir),
         "--color",
         "-b",
         "html",
-        *session.posargs,
+        *s.posargs,
     )
     index = (outdir / "index.html").resolve().as_uri()
-    session.log(f"Documentation is available at {index}")
+    s.log(f"Documentation is available at {index}")
 
 
-@nox.session
-def tests(session: nox.Session) -> None:
-    session.run("uv", "sync", "--active", "--group", "tests")
-    session.run(
-        "pytest", "--cov=coconext", *session.posargs, env={"COCOTB_USER_COVERAGE": "1"}
-    )
+@session(
+    uv_groups=["tests"],
+    uv_sync_locked=False,
+)
+def tests(s: nox.Session) -> None:
+    s.run("pytest", "--cov=coconext", *s.posargs, env={"COCOTB_USER_COVERAGE": "1"})
     coverage_files = glob.glob("**/.coverage", recursive=True)
-    session.run("coverage", "combine", *coverage_files)
-    session.run("coverage", "xml")
-    session.run("coverage", "report")
+    s.run("coverage", "combine", *coverage_files)
+    s.run("coverage", "xml")
+    s.run("coverage", "report")
