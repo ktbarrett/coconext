1f0ce97642584bc1646f5f7135b770f567449fb6
Kaleb Barrett <ktbarrett@hudson-trading.com>
2026-05-17 00:38:29 -0400

    Rename types/array.hpp to types/dynamic_array.hpp
    Pure file rename; no content changes. The existing class is still named
    Array; that rename happens in the next commit alongside the introduction
    of the Array<T> dispatcher alias. types.hpp is updated to point at the
    new path.

--- cpp/include/coconext/types.hpp
+++ cpp/include/coconext/types.hpp
@@ -2,9 +2,9 @@
 #define COCONEXT_TYPES_HPP
 
 // NOLINTBEGIN(unused-includes)
-#include "./types/array.hpp"
 #include "./types/concepts.hpp"
 #include "./types/direction.hpp"
+#include "./types/dynamic_array.hpp"
 #include "./types/logic.hpp"
 #include "./types/range.hpp"
 // NOLINTEND(unused-includes)
