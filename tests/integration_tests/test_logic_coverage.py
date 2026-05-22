280f4bf43dafbc54ab8b135e0afc5ec36670801f
ZiaCheemaGit <ziaurrehman1145@gmail.com>
2026-05-22 09:54:19 +0500

    increase coverage

--- tests/integration_tests/test_logic_coverage.py
+++ tests/integration_tests/test_logic_coverage.py
@@ -6,3 +6,8 @@ from coconext.types import Bit
 def test_bit_str_conversions() -> None:
     assert str(Bit(0)) == "0"
     assert str(Bit(1)) == "1"
+
+
+def test_bit_repr():
+    assert eval(repr(Bit("0"))) == Bit("0")
+    assert eval(repr(Bit("1"))) == Bit("1")
