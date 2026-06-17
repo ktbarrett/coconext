5ccbe415a35b4605e2a75b1b59abf8324c2ab356
ZiaCheemaGit <ziaurrehman1145@gmail.com>
2026-06-17 20:03:01 +0500

    Re-structure nanobind tests
    all test bindings .cpp files are separated per binding and
    all python tests are now in `tests/nanobind/pytest`

---
+++ tests/nanobind/bind_vector.cpp
@@ -0,0 +1,17 @@
+#include "../../nanobind/include/bind_vector.hpp"
+#include <coconext/types/vector.hpp>
+#include <nanobind/nanobind.h>
+#include <string>
+
+namespace nb = nanobind;
+using namespace coconext::types;
+
+void register_range(nb::module_& m);
+
+NB_MODULE(nanobind_tests, m) {
+
+    register_range(m);
+
+    coconext_nb::bind_array<Vector<int>>(m, "IntVector");
+    coconext_nb::bind_array<Vector<std::string>>(m, "StringVector");
+}
