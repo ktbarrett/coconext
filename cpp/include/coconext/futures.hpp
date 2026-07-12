d2baa8ff6165ed2ec09f154f6dce95e1c9e7c685
Kaleb Barrett <dev.ktbarrett@gmail.com>
2026-07-12 17:51:32 -0600

    Refactor Futures to use Deque
    Also remove the callbacks for now.

--- cpp/include/coconext/futures.hpp
+++ cpp/include/coconext/futures.hpp
@@ -1,15 +1,14 @@
 #ifndef COCONEXT_FUTURES_HPP
 #define COCONEXT_FUTURES_HPP
 
+#include "coconext/event_deque.hpp"
 #include <coroutine>
 #include <exception>
-#include <functional>
 #include <optional>
 #include <stdexcept>
 
 #include <coconext/cancelled.hpp>
 #include <coconext/event_loop.hpp>
-#include <vector>
 
 namespace coconext::futures {
 
@@ -18,10 +17,13 @@ class Future;
 
 namespace detail {
 
+// Futures and their Awaiters are tightly coupled, but since this has to be shared with the
+// void overload, it's split out.
 template <typename T>
-class Awaiter {
+class FutureAwaiter : coconext::event_loop::Event {
     friend class Future<T>;
-    explicit Awaiter(Future<T>& future) : future_(future) {}
+
+    explicit FutureAwaiter(Future<T>& future) : future_(future) {}
 
   public:
     bool await_ready() const noexcept { return future_.done(); }
@@ -39,6 +41,7 @@ class Awaiter {
 // Single-shot, multiple-consumer awaitable object.
 template <typename T>
 class Future {
+  public:
   public:
     // Future is not copyable as it represents a single event.
     Future(Future const&) = delete;
@@ -59,43 +62,36 @@ class Future {
     std::exception_ptr exception() const noexcept { return exc_; }
 
   private:
-    void do_callbacks() noexcept {
-        for (auto& cb : callbacks_) {
-            cb();
-        }
+    void schedule_task_resumes() noexcept {
+        // TODO Get EventLoop::Handle and steal_extend_back(deque_)
     }
 
   public:
     void set_result(T const& value) noexcept {
         value_ = value;
-        do_callbacks();
+        schedule_task_resumes();
     }
     void set_exception(std::exception_ptr exc) noexcept {
         exc_ = exc;
-        do_callbacks();
+        schedule_task_resumes();
     }
     void cancel() noexcept {
-        exc_ = std::make_exception_ptr(coconext::Cancelled());
+        exc_ = std::make_exception_ptr(coconext::Cancelled{});
         cancelled_ = true;
-        do_callbacks();
+        schedule_task_resumes();
     }
 
   public:
-    template <typename F>
-    void add_callback(F&& f) {
-        callbacks_.emplace_back(std::forward<F>(f));
-    }
-
-    detail::Awaiter<T> operator co_await() {
+    detail::FutureAwaiter<T> operator co_await() {
         // TODO
-        return detail::Awaiter(*this);
+        return detail::FutureAwaiter<T>(*this);
     }
 
   private:
     std::optional<T> value_ = std::nullopt;
     std::exception_ptr exc_ = nullptr;
     bool cancelled_ = false;
-    std::vector<std::function<void()>> callbacks_;
+    coconext::event_loop::Cmarqueue deque_;
 };
 
 template <>
@@ -120,37 +116,38 @@ class Future<void> {
     std::exception_ptr exception() const noexcept { return exc_; }
 
   private:
-    void do_callbacks() noexcept {
-        for (auto& cb : callbacks_) {
-            cb();
-        }
+    void schedule_task_resumes() noexcept {
+        // TODO Get EventLoop::Handle and steal_extend_back(deque_)
     }
 
   public:
     void set_result() {
         done_ = true;
-        do_callbacks();
+        schedule_task_resumes();
     }
     void set_exception(std::exception_ptr exc) noexcept {
         exc_ = exc;
         done_ = true;
-        do_callbacks();
+        schedule_task_resumes();
     }
     void cancel() noexcept {
         exc_ = std::make_exception_ptr(coconext::Cancelled());
         done_ = true;
         cancelled_ = true;
-        do_callbacks();
+        schedule_task_resumes();
     }
 
   public:
-    detail::Awaiter<void> operator co_await() { return detail::Awaiter<void>(*this); }
+    detail::FutureAwaiter<void> operator co_await() {
+        // TODO
+        return detail::FutureAwaiter<void>(*this);
+    }
 
   private:
     std::exception_ptr exc_ = nullptr;
     bool done_ = false;
     bool cancelled_ = false;
-    std::vector<std::function<void()>> callbacks_;
+    coconext::event_loop::Cmarqueue deque_;
 };
 
 }  // namespace coconext::futures
