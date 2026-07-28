bf29e88ab48f8ac1136d65f4956fac6c5d474b11
Kaleb Barrett <ktbarrett@hudson-trading.com>
2026-07-27 22:21:42 -0400

    Touch up Event Loop

--- cpp/include/coconext/event_loop.hpp
+++ cpp/include/coconext/event_loop.hpp
@@ -27,11 +27,34 @@ class Event {
     }
 };
 
-// An external source of events drives the loop by acquiring a handle, scheduling callbacks,
-// and running the loop until exhaustion before releasing ownership to allow another
-// external event to be handled. The EventLoop does not own the entries; it merely manages
-// their scheduling and invocation. Task lifetime is managed by TaskManagers.
 class EventLoop {
+    // An external source of events drives the loop by acquiring a handle, scheduling
+    // callbacks, and running the loop until exhaustion before releasing ownership to allow
+    // another external event to be handled. The EventLoop does not own the entries; it
+    // merely manages their scheduling and invocation. Task lifetime is managed by
+    // TaskManagers.
+    //
+    // The lock acquisition uses a recursive mutex so multiple external users see
+    // exclusivity, but the events in the event loop can schedule other events, making lock
+    // acquiisition recursive. You might ask why we don't just use a non-recursive mutex and
+    // have Futures and other schedulable things know whether they are recursively scheduled
+    // or not? While this is generally true, and is true in cocotb currently, this buys us 2
+    // things:
+    //
+    //   1. There is a single `fire()` in Future so that subclasses don't need to worry
+    //      about whether they are recursively scheduled or not. Nor do we have to worry
+    //      about exposing more API to service this.
+    //   2. It is possible there are Futures that might be scheduled sometimes recursively
+    //      and sometimes not. For example, a Future is set in a DPI import function.
+    //      Sometimes this function is called directly from the RTL (external event), other
+    //      times a cocotb function calls into a DPI export function, which then calls this
+    //      function (recursive).
+    //
+    // Recursive mutexes are not free, but they are not expensive either. Likely implemented
+    // as an atomic compare of thread_ids.
+    //
+    // The is_running_ flag is used to prevent recursive calls to run() from happening.
+
   public:
     class Handle {
         friend class detail::EventLoop;
@@ -39,7 +62,12 @@ class EventLoop {
       public:
         Handle(Handle&& other) = default;
         ~Handle() {
-            run();
+            // Force the loop to run any remaining events before releasing the lock. We
+            // don't want to leave any events unprocessed for the next external event to
+            // handle.
+            if (!loop_.is_running_) {
+                run_();
+            }
             loop_.mtx_.unlock();
         }
 
@@ -51,9 +79,15 @@ class EventLoop {
         }
 
         void run() {
+            // Recursive calls to run() should never happen.
             if (loop_.is_running_) {
-                return;
+                throw std::runtime_error("EventLoop is already running");
             }
+            run_();
+        }
+
+      private:
+        void run_() {
             loop_.is_running_ = true;
             while (!loop_.queue_.empty()) {
                 auto event = loop_.queue_.pop_front();
@@ -62,7 +96,6 @@ class EventLoop {
             loop_.is_running_ = false;
         }
 
-      private:
         Handle(detail::EventLoop& loop) : loop_(loop) {}
 
         detail::EventLoop& loop_;
